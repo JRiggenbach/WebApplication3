d194a4fe63872558b4935e6acd031bbda0e83486
Josh Riggenbach <jriggenbach@gmail.com>
2014-04-22 18:43:14 -0400

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Trace.WriteLine("Azure HOLs Rule!");
+            Trace.WriteLine("Azure HOLs Rule!");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
